110a32599a516de26843084a2b27f692b4664640
Keerthana-163 <140970060+Keerthana-163@users.noreply.github.com>
2025-07-25 13:30:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,6 @@ def get_image(filename):
     return send_from_directory('static/images', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)  # Or any other available port
+
 
